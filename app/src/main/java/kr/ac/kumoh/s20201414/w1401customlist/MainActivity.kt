c39e91e7f1ef970b5e82537692335c8a8617d0e6
jeongchan12 <jkc9709@hanamail.net>
2022-12-05 16:27:46 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20201414/w1401customlist/MainActivity.kt
@@ -0,0 +1,65 @@
+package kr.ac.kumoh.s20201414.w1401customlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.kumoh.s20201414.w1401customlist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: SongViewModel
+    private val songAdapter = SongAdapter()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+
+        binding.listSong.apply {
+            layoutManager = LinearLayoutManager(application)
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = songAdapter
+        }
+
+        model.list.observe(this){
+            songAdapter.notifyItemRangeInserted(0, songAdapter.itemCount)
+        }
+
+        model.requestSong()
+    }
+    inner class SongAdapter : RecyclerView.Adapter<SongAdapter.ViewHolder>(){
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            //과제때 싹 고쳐야함, 애트리뷰트 갯수만큼 필요
+            //val txText = itemView.findViewById(android.R.id.text1) as TextView
+            //val txText: TextView = itemView.findViewById(android.R.id.text1)
+            val txTitle = itemView.findViewById<TextView>(android.R.id.text1)
+            val txSinger = itemView.findViewById<TextView>(android.R.id.text2)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            //통나무 생성할 때 사용
+            val view = layoutInflater.inflate(
+                android.R.layout.simple_list_item_2, //만든 레이아웃에 맞춰서 바꿔야 함
+                parent,
+                false
+            )
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            //통나무의 내용을 세팅하는 곳(새로 나오는 부분의) ViewHolder와 세트이기에 같은 개수만큼 변경
+            holder.txTitle.text = model.list.value?.get(position)?.title.toString()
+            holder.txSinger.text = model.list.value?.get(position)?.singer.toString()
+        }
+
+        override fun getItemCount() = model.list.value?.size ?: 0
+    }
+}
